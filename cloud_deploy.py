18a6a3c6ee477910705a14256886331fecae155e
josecabrales00 <jcabrales@alumnos.uai.cl>
2026-06-23 13:23:18 -0400

    v1.1.2 - Vercel Build Fix - Add cloud_deploy.py

--- cloud_deploy.py
+++ cloud_deploy.py
@@ -41,7 +41,7 @@ def deploy():
         file_url = f"https://api.github.com/repos/{username}/{REPO_NAME}/contents/{f}"
         file_res = requests.get(file_url, headers=gh_headers)
         payload = {
-            "message": f"v1.1.1 - Critical Bugfixes - Add {f}",
+            "message": f"v1.1.2 - Vercel Build Fix - Add {f}",
             "content": base64.b64encode(content).decode('utf-8')
         }
         if file_res.ok:
