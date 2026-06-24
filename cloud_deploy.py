ebaf0b8732463a57219798694d46d078ba2592fc
josecabrales00 <jcabrales@alumnos.uai.cl>
2026-06-24 18:33:29 -0400

    v1.3.3 - Local Scraper Fix - cloud_deploy.py

--- cloud_deploy.py
+++ cloud_deploy.py
@@ -52,7 +52,7 @@ def deploy():
         file_url = f"https://api.github.com/repos/{username}/{REPO_NAME}/contents/{f}"
         file_res = requests.get(file_url, headers=gh_headers)
         payload = {
-            "message": f"v1.3.2 - Hybrid Brain & Beautiful Report - {f}",
+            "message": f"v1.3.3 - Local Scraper Fix - {f}",
             "content": base64.b64encode(content).decode('utf-8')
         }
         if file_res.ok:
